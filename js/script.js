bb913eeb47206d427b350267ebb57b5e90bc5792
Gavin <waltersg298@gmail.com>
2022-12-16 17:25:51 -0700

    added modal for login/register -- still need to build out modal interactions and capabilities

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 
 // Get the modal
 var modal = document.getElementById("taskModal");
+var modal2 = document.getElementById("loginRegisterModal");
 
 // Get the button that opens the modal
 var btn = document.getElementById("taskBtn");
+var btn2 = document.getElementById("loginRegisterBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -13,14 +15,20 @@ btn.onclick = function() {
   modal.style.display = "block";
 }
 
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+  modal2.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
+  if (event.target == modal || event.target == modal2) {
     modal.style.display = "none";
+    modal2.style.display = "none";
   }
 }
